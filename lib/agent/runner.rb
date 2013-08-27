9cb82b0dde670637e91be21f13ff119633342f25
Brian Goff <cpuguy83@gmail.com>
2013-08-26 22:58:31 -0400

    Fire up Scheduler

--- lib/agent/runner.rb
+++ lib/agent/runner.rb
@@ -1,7 +1,9 @@
 require 'agent/worker'
+require 'agent/scheduler'
 
 module Agent
   class Runner < Celluloid::SupervisionGroup
     supervise Agent::Worker, as: :worker, size: 10
+    supervise Agent::Scheduler, as: :scheduler
   end
 end
