364078146f52d16c9442ef7c6a51cdbc3655b83a
Brian Goff <cpuguy83@gmail.com>
2013-08-27 09:15:49 -0400

    add actual do block to the describe statement (somehow missed the block)

--- spec/agent/relation_proxy_spec.rb
+++ spec/agent/relation_proxy_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 Foo = Struct.new(:bar)
 
 describe Agent::RelationProxy do
-  describe '.build'
-  Given(:proxy) { Agent::RelationProxy.new(Foo, nil) }
-  When(:result) { proxy.build }
-  Then { expect(result).to be_a Foo }
-  Then { expect(proxy.collection.count).to be 1 }
+  describe '.build' do
+    Given(:proxy) { Agent::RelationProxy.new(Foo, nil) }
+    When(:result) { proxy.build }
+    Then { expect(result).to be_a Foo }
+    Then { expect(proxy.collection.count).to be 1 }
+  end
 end
