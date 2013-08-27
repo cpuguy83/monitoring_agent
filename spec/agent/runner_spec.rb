8dbc2b4920512db656291b8eaefb00a500c11d2f
Brian Goff <cpuguy83@gmail.com>
2013-08-27 08:57:09 -0400

    Add test to verify scheduler actor was fired up

--- spec/agent/runner_spec.rb
+++ spec/agent/runner_spec.rb
@@ -4,5 +4,6 @@ describe Agent::Runner do
   describe '.run' do
     When { Agent::Runner.run! }
     Then { expect(Celluloid::Actor[:worker]).to be_an Agent::Worker  }
+    Then { expect(Celluloid::Actor[:scheduler]).to be_an Agent::Scheduler }
   end
 end
