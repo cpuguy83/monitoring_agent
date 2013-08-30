2f1e8a0107754b2717a0ea616cbf6ae527e84eff
Brian Goff <cpuguy83@gmail.com>
2013-08-29 23:13:52 -0400

    Add basic Worker logic

--- lib/agent/worker.rb
+++ lib/agent/worker.rb
@@ -1,5 +1,51 @@
 module Agent
   class Worker
+    class OptionError < StandardError; end
+
     include Celluloid
+
+    attr_reader :work_class, :work_method, :work_arguments,
+      :handler_class, :handler_method
+
+    def perform(opts={})
+      set_work_options(opts)
+
+      output = if work_arguments
+        perform_with_arguments
+      else
+        perform_without_arguments
+      end
+
+      run_handler(output) if handler
+    end
+
+  private
+
+    def set_work_options(opts={})
+      [:work_class, :work_method].each do |opt|
+        raise OptionError, "Missing option #{opt}" unless opts[opt]
+      end
+
+      @work_class = opts[:work_class]
+      @work_method = opts[:work_method] || :perform
+      @work_arguments = opts[:work_arguments]
+      @handler_class = opts[:handler_class] || work_class
+      @handler_method = opts[:handleer_method] || :handle
+    end
+
+    def run_handler(output)
+      if handler_class.respond_to?(handler_method)
+        handler_class.public_send(handler_method, output)
+      end
+    end
+
+
+    def perform_with_arguments
+      work_class.public_send(work_method, *work_arguments)
+    end
+
+    def perform_without_arguments
+      work_class.public_send(work_method)
+    end
   end
 end
