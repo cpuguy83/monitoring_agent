238123ebc34a5341b13b4af48e817ea4a71bb620
Brian Goff <cpuguy83@gmail.com>
2013-08-29 22:50:33 -0400

    Add methods to load clockwork config and fire it up

--- lib/agent/scheduler.rb
+++ lib/agent/scheduler.rb
@@ -2,5 +2,22 @@ module Agent
   class Scheduler
     include Celluloid
     include Clockwork
+
+    def start
+      load_config
+      configure_clockwork
+      run
+    end
+
+    def configure_clockwork
+      configure do |c|
+        c[:logger] = ::Logger.new('/tmp/test.log')
+      end
+    end
+  private
+    def load_config
+      file = File.expand_path("../../../config/schedule.rb", __FILE__)
+      eval(File.open(file).read) if File.exists? file
+    end
   end
 end
