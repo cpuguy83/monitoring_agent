f026dbadd3f926adf65fb71cfc067d8ffc2b40fc
Brian Goff <cpuguy83@gmail.com>
2013-09-02 17:53:57 -0400

    Add work queue

--- lib/agent/runner.rb
+++ lib/agent/runner.rb
@@ -1,10 +1,12 @@
 require 'agent/worker'
 require 'agent/scheduler'
+require 'agent/work_queue'
 
 module Agent
   class Runner < Celluloid::SupervisionGroup
     pool Agent::Worker, as: :worker, size: 10
     supervise Agent::Scheduler, as: :scheduler
+    supervise Agent::WorkQueue, as: :work_queue
 
     def worker
       Celluloid::Actor[:worker]
