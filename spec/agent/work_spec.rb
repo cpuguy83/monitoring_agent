60e3723c48931dc6f4adcb3b172a3d4c6bb86af1
Brian Goff <cpuguy83@gmail.com>
2013-09-02 18:09:25 -0400

    Check equality method

--- spec/agent/work_spec.rb
+++ spec/agent/work_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Agent::Work do
-
+  Given(:work) { Agent::Work.new work_class: 'Foo' }
+  Given(:work2) { Agent::Work.new work_class: 'Bar' }
+  describe '==' do
+    When(:result) { [work, work, work2].uniq! }
+    Then { expect(result.count).to be 2 }
+  end
 end
