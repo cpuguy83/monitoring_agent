93e4ee3d2df44c334bdf8d5b68337d2d3edbeb1c
Brian Goff <cpuguy83@gmail.com>
2013-09-02 23:26:20 -0400

    Prep Agent::Work to handle storing info about check times so we can eliminate the clockwork gem

--- lib/agent/work.rb
+++ lib/agent/work.rb
@@ -2,7 +2,7 @@ module Agent
   class Work
     attr_reader :attributes
     def self.instance_attrs
-      [:work_class, :arguments, :perform_at]
+      [:work_class, :arguments, :perform_at, :frequency, :last_run, :output]
     end
     def initialize(attrs={})
       @attributes = {}
@@ -12,6 +12,8 @@ module Agent
           public_send("#{key}=", value)
         end
       end
+
+      perform_at ||= Time.new(0)
     end
 
     instance_attrs.each do |attr|
@@ -29,5 +31,32 @@ module Agent
         other.attributes == self.attributes
     end
 
+    def work_now?
+      if perform_at && frequency
+        perform_at_less_than_now? && stale?
+      elsif peform_at && !frequency
+        perform_at_less_than_now?
+      elsif frequency && !perform_at
+        stale?
+      else
+        true
+      end
+    end
+  private
+    def time_since_last_run
+      Time.now - last_run
+    end
+
+    def perform_at_less_than_now?
+      if perform_at.respond_to? :each
+        perform_at.find { |at| at < Time.now }
+      else
+        perform_at < Time.now
+      end
+    end
+
+    def stale?
+      time_since_last_run >= frequency
+    end
   end
 end
