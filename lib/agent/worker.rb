ec91ddedd68799dcec0c10d7617336b5adf1b576
Brian Goff <cpuguy83@gmail.com>
2013-09-02 23:27:04 -0400

    Hook worker up to handle Work objects

--- lib/agent/worker.rb
+++ lib/agent/worker.rb
@@ -4,35 +4,33 @@ module Agent
 
     include Celluloid
 
-    attr_reader :work_class, :work_method, :work_arguments
+    attr_reader :work_class, :work_args
 
-    def perform(opts={})
-      set_work_options(opts)
-
-      output =  if work_arguments
+    def perform(work)
+      set_work_options(work)
+      output =  if work.arguments
                   perform_with_arguments
                 else
                   perform_without_arguments
                 end
-      clear_worker_options
+
+    ensure
+      clear_work_options
     end
 
   private
-
-    def set_work_options(opts={})
-      raise OptionError, "Missing work_class option" unless opts[:work_class]
-
-      @work_class = opts[:work_class]
-      @work_arguments = opts[:work_arguments]
+    def set_work_options(work)
+      @work_class = work.work_class
+      @work_args  = work.arguments
     end
 
     def clear_work_options
       @work_class = nil
-      @work_arguments = nil
+      @work_args = nil
     end
 
     def perform_with_arguments
-      work_class.perform(work_arguments)
+      work_class.perform(work_args)
     end
 
     def perform_without_arguments
