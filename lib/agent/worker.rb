a8ecb1c6a424e1d2fc2a72ffb309e4db5b275d85
Brian Goff <cpuguy83@gmail.com>
2013-09-06 23:34:58 -0400

    Simplified, moved main calls to work_class to the actual work object

--- lib/agent/worker.rb
+++ lib/agent/worker.rb
@@ -1,40 +1,19 @@
 module Agent
   class Worker
-    class OptionError < StandardError; end
 
     include Celluloid
 
-
     def perform(work)
-      set_work_options(work)
-      work.output =  if work.arguments
-                  perform_with_arguments
-                else
-                  perform_without_arguments
-                end
-
+      work.perform
     ensure
-      clear_work_options
+      work.last_run = Time.now
+      put_back(work)
     end
 
   private
-
-    def set_work_options(work)
-      @work_class = work.work_class
-      @work_args  = work.arguments
+    def put_back(work)
+      Actor[:work_schedule].put_back(work)
     end
 
-    def clear_work_options
-      @work_class = nil
-      @work_args = nil
-    end
-
-    def perform_with_arguments
-      work_class.perform(work_args)
-    end
-
-    def perform_without_arguments
-      work_class.perform
-    end
   end
 end
