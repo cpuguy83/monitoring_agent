52c9e3243d3e7f1a59f06ef52c3c667a0dc7a529
Brian Goff <cpuguy83@gmail.com>
2013-09-06 23:31:19 -0400

    Move configuration stuff into it's own class

--- lib/agent/scheduler.rb
+++ lib/agent/scheduler.rb
@@ -6,6 +6,28 @@ module Agent
     def initialize
       super
       async.start
+    class Configuration
+      def initialize
+        load_config
+      end
+
+      def work(name=nil, &block)
+        work = Agent::Work.new
+        work.name = name if name
+        work.instance_eval(&block)
+
+        work.save
+
+        work
+      end
+      alias_method :check, :work
+
+
+    private
+      def load_config
+        file = File.expand_path("../../../config/schedule.rb", __FILE__)
+        eval(File.open(file).read) if File.exists? file
+      end
     end
 
     def start
@@ -21,8 +43,8 @@ module Agent
     end
   private
     def load_config
-      file = File.expand_path("../../../config/schedule.rb", __FILE__)
-      eval(File.open(file).read) if File.exists? file
+      Configuration.new
     end
+
   end
 end
