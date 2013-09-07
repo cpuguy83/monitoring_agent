3fd3eb0e90550fb3cfd73e9eff17b4b2d7dd0333
Brian Goff <cpuguy83@gmail.com>
2013-09-06 23:28:53 -0400

    Use short class names

--- lib/agent/runner.rb
+++ lib/agent/runner.rb
@@ -1,16 +1,16 @@
 require 'agent/worker'
 require 'agent/scheduler'
-require 'agent/work_queue'
+require 'agent/work_schedule'
 
 module Agent
   class Runner < Celluloid::SupervisionGroup
     pool Agent::Worker, as: :worker, size: 10
-    supervise Agent::WorkQueue, as: :work_queue
+    supervise Agent::WorkSchedule, as: :work_schedule
     supervise Agent::Scheduler, as: :scheduler
 
-    [:work_queue, :scheduler, :worker].each do |actor|
+    [:work_schedule, :scheduler, :worker].each do |actor|
       define_method actor do
-        Celluloid::Actor[actor]
+        Actor[actor]
       end
     end
   end
