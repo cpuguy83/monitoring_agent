a11d281f2c578ef525493ebdf651067f53059088
Brian Goff <cpuguy83@gmail.com>
2013-09-06 23:34:22 -0400

    Added work_now? tests

--- spec/agent/work_spec.rb
+++ spec/agent/work_spec.rb
@@ -7,4 +7,33 @@ describe Agent::Work do
     When(:result) { [work, work, work2].uniq! }
     Then { expect(result.count).to be 2 }
   end
+
+  describe '.work_now?' do
+    context 'work is scheduled normally' do
+      Given { work.last_run = 1000.hours.ago }
+      Given { work.frequency = 1.minute }
+      When(:result) { work.work_now? }
+      Then { expect(result).to be true }
+    end
+
+    context 'work is scheduled before it should be' do
+      Given { work.frequency = 1000.hours }
+      Given { work.last_run = 1.minute.ago }
+      When(:result) { work.work_now? }
+      Then { expect(result).to be false }
+    end
+
+    context 'scheduled to run in the past' do
+      Given { work.perform_at = 1.minute.ago }
+      When(:result) { work.work_now? }
+      Then { expect(result).to be true }
+    end
+
+    context 'work is scheduled for the future' do
+      Given { work.frequency = nil }
+      Given { work.perform_at = 100.hours.from_now }
+      When(:result) { work.work_now? }
+      Then { expect(result).to be false }
+    end
+  end
 end
