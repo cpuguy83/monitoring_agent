56d97c670a6675495b44fa87cad8d8713e170927
Brian Goff <cpuguy83@gmail.com>
2013-09-10 22:10:51 -0400

    Updated for testing many jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,17 @@
+require 'open-uri'
 class MyWorker
   include Agent::CheckAgent
-  def perform
-    Logger.new('/tmp/test2.log').info('Stuff')
+  def perform(n)
+    ::Logger.new('/tmp/test2.log').info("Testing #{n}")
+    Kernel.sleep 10
+  end
+end
+counter = 0
+40.times do
+  work "work#{counter}" do
+    frequency 5.seconds
+    work_class MyWorker
+    arguments counter +=1
   end
 end
 
-work 'alias' do
-  frequency 5.seconds
-  work_class MyWorker
-end
