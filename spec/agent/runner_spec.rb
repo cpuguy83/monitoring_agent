24d1f567976aa26bdb1c9995ae5f3a5237e37a81
Brian Goff <cpuguy83@gmail.com>
2013-09-26 09:46:12 -0400

    Use new registry access

--- spec/agent/runner_spec.rb
+++ spec/agent/runner_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Agent::Runner do
   describe '.run' do
     When { Agent::Runner.run! }
-    Then { expect(Celluloid::Actor[:worker]).to be_an Agent::Worker  }
-    Then { expect(Celluloid::Actor[:scheduler]).to be_an Agent::Scheduler }
+    Then { expect(Agent.runner[:worker]).to be_an Agent::Worker  }
+    Then { expect(Agent.runner[:scheduler]).to be_an Agent::Scheduler }
   end
 end
