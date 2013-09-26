adbd462fc5ca849b1b1ffe9ec79cfcda9445edc2
Brian Goff <cpuguy83@gmail.com>
2013-09-26 09:43:22 -0400

    Add similar attar logic to service as host

--- lib/agent/host.rb
+++ lib/agent/host.rb
@@ -1,6 +1,23 @@
 module Agent
   class Host
-    class Service; end
+    class Service
+      def initialize(attrs={})
+        relation = attrs[:relation_proxy]
+        @host = relation.parent_object
+        attrs.delete(:relation_proxy)
+        @attributes = attrs
+      end
+
+      def method_missing(method_name, arg=nil)
+        if method_name.to_s =~ /=$/
+          method_name = method_name.to_s.gsub(/=$/, '').to_sym
+          @attributes.send(:[]=, method_name, arg)
+        else
+          attributes.fetch(method_name.to_sym) { nil }
+        end
+      end
+    end
+
     attr_reader :services, :hosts, :attributes
 
     def initialize(attrs={})
