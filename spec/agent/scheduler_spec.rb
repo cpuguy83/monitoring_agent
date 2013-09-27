b58048c78e8c9b72a518e89c7f7e28fe646f3865
Brian Goff <cpuguy83@gmail.com>
2013-09-26 22:40:02 -0400

    Add required stubs

--- spec/agent/scheduler_spec.rb
+++ spec/agent/scheduler_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 module Agent
   describe Scheduler do
+    before :each do
+      runner = double(:runner)
+      Agent.stub(:runner).and_return(runner)
+      allow(runner).to receive(:[]).with(:scheduler).and_return(Agent::Scheduler.new)
+    end
     Given(:scheduler) { Agent.runner[:scheduler] }
   end
 end
