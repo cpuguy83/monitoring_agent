c7b33918bf8290be06bd88161579b9c7b8302ad8
Brian Goff <cpuguy83@gmail.com>
2013-09-26 22:39:33 -0400

    Clean up how actors are accessed

--- spec/agent/runner_spec.rb
+++ spec/agent/runner_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Agent::Runner do
   describe '.run' do
-    When { Agent::Runner.run! }
-    Then { expect(Agent.runner[:worker]).to be_an Agent::Worker  }
-    Then { expect(Agent.runner[:scheduler]).to be_an Agent::Scheduler }
+    When(:runner) { Agent::Runner.run! }
+    Then { expect(runner[:worker]).to be_an Agent::Worker  }
+    Then { expect(runner[:scheduler]).to be_an Agent::Scheduler }
   end
 end
