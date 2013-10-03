7ba7c63d1097f3d08b8e3d7806bc67759c3a9088
Brian Goff <cpuguy83@gmail.com>
2013-10-03 09:33:54 -0400

    Remove duplicate tests

--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -2,19 +2,15 @@ require 'spec_helper'
 
 describe Agent do
   describe '.start' do
-    When(:result) { Agent.start! }
+    Given { Agent.start! }
+    When(:result) { Agent.runner }
     Then { expect(result).to be_an Agent::Runner }
+    And { expect(result).to be_alive }
   end
 
   describe '.stop' do
     Given { Agent.start! }
     When { Agent.stop }
-    Then { expect(Agent.runner).to be nil}
-  end
-
-  describe '.runner' do
-    Given { Agent.start! }
-    Then { expect(Agent.runner).to be_a Agent::Runner }
-    Then { expect(Agent.runner).to be_alive }
+    Then { expect(Agent.runner).to be_nil }
   end
 end
