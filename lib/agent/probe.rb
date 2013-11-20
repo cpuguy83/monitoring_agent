4c5d90999561ff90474598319ed310ebde5da641
Brian Goff <cpuguy83@gmail.com>
2013-11-20 10:28:13 -0500

    Fix method names in Agent::Probe

--- lib/agent/probe.rb
+++ lib/agent/probe.rb
@@ -1,29 +1,28 @@
 module Agent
   module Probe
-
     def self.included(base)
       base.extend(ClassMethods)
       base.instance_eval do
-        private_class_method :call_handler, :get_instance
+        private_class_method :call_handler, :instance
         attr_accessor :output, :args
       end
     end
 
     module ClassMethods
       def perform(*args)
-        probe = get_instance(*args)
-        probe.output = check_agent.perform(*args)
+        probe = instance(*args)
+        probe.output = probe.perform(*args)
         call_handler(probe)
       end
 
-      def get_instance(*args)
+      def instance(*args)
         instance = new
         instance.args = args
         instance
       end
 
       def call_handler(probe)
-        check_agent.handle if check_agent.respond_to?(:handle)
+        probe.handle if probe.respond_to?(:handle)
       end
     end
   end
