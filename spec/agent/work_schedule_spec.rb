0f1200e490e64467b90815857da12f264c107240
Brian Goff <cpuguy83@gmail.com>
2013-11-28 20:06:40 -0500

    Add required attrs so we don’t raise exception

--- spec/agent/work_schedule_spec.rb
+++ spec/agent/work_schedule_spec.rb
@@ -11,7 +11,7 @@ module Agent
     end
 
     Given(:queue) { Agent.runner[:work_schedule] }
-    Given(:work) { Work.new }
+    Given(:work) { WorkTest.new(name: 'foo', work_class: 'bar') }
     describe '.add' do
       context 'Work is added' do
         When { queue.add(work) }
