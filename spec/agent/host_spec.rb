d67b8a18239024f34439389a91a2e3a2bef883a9
Brian Goff <cpuguy83@gmail.com>
2013-11-28 20:04:30 -0500

    Not taking a build argument here anymore

--- spec/agent/host_spec.rb
+++ spec/agent/host_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Agent::Host do
   describe '.services' do
     Given(:host) { Agent::Host.new }
-    When { host.services.build }
     Then { expect(host.services).to respond_to :count}
     Then { expect(host.services).to respond_to :each }
   end
