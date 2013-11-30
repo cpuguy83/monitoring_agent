50377be970a09f27fe016fb3c35ecaa415dbd05f
Brian Goff <cpuguy83@gmail.com>
2013-11-30 12:07:57 -0500

    Don’t need these specs right now

--- spec/agent/runner_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Agent::Runner do
-  describe '.run' do
-    When(:runner) { Agent::Runner.run! }
-    Then { expect(runner[:worker]).to be_an Agent::Worker  }
-    Then { expect(runner[:scheduler]).to be_an Agent::Scheduler }
-  end
-end
